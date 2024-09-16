7e74e672221814cab55b512a681b0a3a04d8cc2e
pawn-4-git <pawn4sns+dummy@gmail.com>
2024-09-16 10:52:11 +0900

    タスク状況整理機能

--- script/taskView.js
+++ script/taskView.js
@@ -16,7 +16,7 @@ function taskViewFuction() {
     const list = getList();
     let total = getTotalTaskCount(list);
     let taskTitleDiv = document.createElement("h2");
-    taskTitleDiv.innerText = "状況確認"
+    taskTitleDiv.innerText = "状況確認(課題数)"
     categoryTaskDialog.appendChild(taskTitleDiv);
     if (total > 0) {
         let text = '';
@@ -45,53 +45,59 @@ function taskViewFuction() {
         } else {
             totalPercent = 0;
         }
-        for (let i = 0; i < list.length; i++) {
-            let color = getIconColor(list[i]);
-            let count = getTaskCount(list[i]);
-            let title = gettitle(list[i]);
-            if (title != '処理済み' && title != '完了' && count > 0) {
-                let percent = Math.floor(count * 100 / total);
-                if (percent < 1) {
-                    percent = 1;
-                }
-                let circle = document.createElementNS(svg, 'circle');
-                circle.style.setProperty('stroke', color);
-                circle.style.setProperty('animation', 'percent' + totalPercent + ' 2s ease-in-out forwards');
-                circle.classList.add('circleClass');
-                circle.setAttribute('cx', cx);
-                circle.setAttribute('cy', cy);
-                circle.setAttribute('r', r);
-                taskCircle.appendChild(circle);
-                if (percent > 0) {
-                    text = text + '<span class="taskDiscription" style="color:' + color + ';">' + title + '・・・' + Math.floor(count * 100 / total) + "%</span><br/>"
-                }
-                totalPercent += percent;
-                if (totalPercent > 100) {
-                    totalPercent = 100;
-                }
+        if (notClosePercent > 0) {
+            for (let i = 0; i < list.length; i++) {
+                let color = getIconColor(list[i]);
+                let count = getTaskCount(list[i]);
+                let title = gettitle(list[i]);
+                if (title != '処理済み' && title != '完了' && count > 0) {
+                    let percent = Math.floor(count * 100 / total);
+                    if (percent < 1) {
+                        percent = 1;
+                    }
+                    let circle = document.createElementNS(svg, 'circle');
+                    circle.style.setProperty('stroke', color);
+                    circle.style.setProperty('animation', 'percent' + totalPercent + ' 2s ease-in-out forwards');
+                    circle.classList.add('circleClass');
+                    circle.setAttribute('cx', cx);
+                    circle.setAttribute('cy', cy);
+                    circle.setAttribute('r', r);
+                    taskCircle.appendChild(circle);
+                    if (percent > 0) {
+                        text = text + '<span class="taskDiscription" style="color:' + color + ';">' + title + '・・・' + Math.floor(count * 100 / total) + "%</span><br/>"
+                    }
+                    totalPercent += percent;
+                    if (totalPercent > 100) {
+                        totalPercent = 100;
+                    }
 
+                }
+                console.log(gettitle(list[i]));
             }
-            console.log(gettitle(list[i]));
+            const animate = document.createElementNS(svg, "animate");
+            animate.setAttribute("attributeName", "r");
+            animate.setAttribute("from", "50");
+            animate.setAttribute("to", "100");
+            animate.setAttribute("dur", "2s");
+            animate.setAttribute("fill", "freeze");
+            animate.setAttribute("repeatCount", "1");
+            taskCircle.appendChild(animate);
+            taskViewDiv.appendChild(taskCircle);
+            taskDiv.appendChild(taskViewDiv)
+            let taskDataDiv = document.createElement("div");
+            taskDataDiv.innerHTML = text
+            taskDiv.appendChild(taskDataDiv);
+
+            categoryTaskDialog.appendChild(taskDiv);
+            taskCircle.display = 'block'
+            animate.beginElement();
+        } else {
+            let taskDataDiv = document.createElement("div");
+            taskDataDiv.innerText = "課題がありません"
+            taskDiv.appendChild(taskDataDiv);
+            categoryTaskDialog.appendChild(taskDiv);
         }
-        const animate = document.createElementNS(svg, "animate");
-        animate.setAttribute("attributeName", "r");
-        animate.setAttribute("from", "50");
-        animate.setAttribute("to", "100");
-        animate.setAttribute("dur", "2s");
-        animate.setAttribute("fill", "freeze");
-        animate.setAttribute("repeatCount", "1");
-        taskCircle.appendChild(animate);
-        taskViewDiv.appendChild(taskCircle);
-        taskDiv.appendChild(taskViewDiv)
-        let taskDataDiv = document.createElement("div");
-        taskDataDiv.innerHTML = text
-        taskDiv.appendChild(taskDataDiv);
-
-        categoryTaskDialog.appendChild(taskDiv);
-        taskCircle.display = 'block'
-        animate.beginElement();
     } else {
-
         let taskDataDiv = document.createElement("div");
         taskDataDiv.innerText = "課題がありません"
         taskDiv.appendChild(taskDataDiv);
